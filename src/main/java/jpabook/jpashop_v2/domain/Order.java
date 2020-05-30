964c5c9e91ca88394ef5208a5290a1ab5ae8e27b
liki <>
2020-05-30 09:13:13 +0900

    readme 보완, lazy -> static 으로 변경

--- src/main/java/jpabook/jpashop_v2/domain/Order.java
+++ src/main/java/jpabook/jpashop_v2/domain/Order.java
@@ -5,6 +5,9 @@ import javax.persistence.*;
 import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
+
+import static javax.persistence.FetchType.LAZY;
+
 @Entity
 @Table(name = "orders")
 @Getter @Setter
@@ -13,17 +16,17 @@ public class Order {
     @Column(name = "order_id")
     private Long id;
 
-    @ManyToOne(fetch = FetchType.LAZY)
+    @ManyToOne(fetch = LAZY)
     @JoinColumn(name = "member_id")
     private Member member; //주문 회원
 
     @OneToMany(mappedBy = "order", cascade = CascadeType.ALL)
     private List<OrderItem> orderItems = new ArrayList<>();
 
-    @OneToOne(cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToOne(cascade = CascadeType.ALL, fetch = LAZY)
     @JoinColumn(name = "delivery_id")
     private Delivery delivery; //배송정보
-
+    //orderDate -> order_date 이렇게 바꾼다.(내부적으로)
     private LocalDateTime orderDate; //주문시간
 
     @Enumerated(EnumType.STRING)
